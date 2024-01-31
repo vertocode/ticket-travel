1507fb0a699719b1baef52ad67ebac36b46d19b9
vertocode <evertonvanoni1@gmail.com>
2024-01-31 17:31:31 -0300

    feat: add type of properties in filters

--- components/Filters/Amenities/Amenities.tsx
+++ components/Filters/Amenities/Amenities.tsx
@@ -1,9 +1,27 @@
 import {ReactElement} from 'react'
+import { Checkbox } from '@mui/material'
+import '@/styles/components/Filters/Amenities/Amenities.scss'
 
 const Amenities = (): ReactElement => {
+    const options = [
+        'Wi-Fi',
+        'Cozinha',
+        'Máquina de Lavar',
+        'Ar-condicionado',
+        'Secadora'
+    ]
+
     return (
-        <div className="Amenities">
-            <h1>Amenities</h1>
+        <div className="amenities">
+            <h3 className="title">Comodidades</h3>
+            <div className="options">
+                {options.map((option, index) => (
+                    <div key={index}>
+                        <Checkbox />
+                        <span>{option}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- components/Filters/TypeOfProperty/TypeOfProperty.tsx
+++ components/Filters/TypeOfProperty/TypeOfProperty.tsx
@@ -1,9 +1,26 @@
 import {ReactElement} from 'react'
+import Image from 'next/image'
+import '@/styles/components/Filters/TypeOfProperty/TypeOfProperty.scss'
 
 const TypeOfProperty = (): ReactElement => {
+    const typeOfPropertyConfig = [
+        { type: 'casa', quantity: '346', src: '/imgs/casa.svg', active: true },
+        { type: 'Apartamento', quantity: '234', src: '/imgs/apartamento.svg' },
+        { type: 'Hotel', quantity: '23', src: '/imgs/hotel.svg' },
+    ]
+
     return (
-        <div className="TypeOfProperty">
-            <h1>TypeOfProperty</h1>
+        <div className="type-of-property">
+            <h3 className="title">Tipo de propriedade</h3>
+            <div className="types">
+                {typeOfPropertyConfig.map((typeOfProperty, index) => (
+                    <div key={index} className={`type-button ${typeOfProperty?.active ? 'active' : ''}`}>
+                        <Image height={ 24 } width={ 24 } src={typeOfProperty.src} alt={typeOfProperty.type} />
+                        <span className="type">{typeOfProperty.type}</span>
+                        <span className="quantity">({typeOfProperty.quantity})</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
