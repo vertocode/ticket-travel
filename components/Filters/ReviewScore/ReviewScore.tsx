d783dbb80ba0fc30508e2ff841dc692702974bf3
vertocode <evertonvanoni1@gmail.com>
2024-01-31 18:01:44 -0300

    feat: add review score section of filters

--- components/Filters/ReviewScore/ReviewScore.tsx
+++ components/Filters/ReviewScore/ReviewScore.tsx
@@ -1,9 +1,32 @@
 import {ReactElement} from 'react'
+import '@/styles/components/Filters/ReviewScore/ReviewScore.scss'
 
 const ReviewScore = (): ReactElement => {
+    const reviewScoreConfig = [
+        { reviewScore: 9, label: 'Excelente', quantity: '543' },
+        { reviewScore: 8, label: 'Muito bom', quantity: '543' },
+        { reviewScore: 7, label: 'Bom', quantity: '543' },
+        { reviewScore: 6, label: 'Ruim', quantity: '543' },
+        { reviewScore: 0, label: 'Pessímo', quantity: '14' },
+    ]
+
     return (
-        <div className="Reviews">
-            <h1>Reviews</h1>
+        <div className="review-score">
+            <h3 className="title">Review Score</h3>
+            <div className="reviews">
+                {reviewScoreConfig.map((reviewScore, index) => (
+                    <div key={index} className="review">
+                        <div className="score-container">
+                            <span
+                                className="score-progress"
+                                style={{ width: `${(reviewScore.reviewScore || 1) * 8 }%` }}
+                            >{reviewScore.reviewScore > 0 && reviewScore.reviewScore}
+                            </span>
+                        </div>
+                        <span className="label">{reviewScore.label} ({reviewScore.quantity})</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
