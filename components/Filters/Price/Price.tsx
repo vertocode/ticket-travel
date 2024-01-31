a93bd35ecf93f5a4e083e41bf19d1662b2e8314a
vertocode <evertonvanoni1@gmail.com>
2024-01-31 16:34:02 -0300

    feat: add price in filters

--- components/Filters/Price/Price.tsx
+++ components/Filters/Price/Price.tsx
@@ -1,9 +1,24 @@
 import {ReactElement} from 'react'
+import '@/styles/components/Filters/Price/Price.scss'
 
 const Price = (): ReactElement => {
+    const prices = [
+        { startPrice: 'R$ 10,00', endPrice: 'R$ 770,00', active: true },
+        { startPrice: 'R$ 10,00', endPrice: 'R$ 770,00' },
+        { startPrice: 'R$ 10,00', endPrice: 'R$ 770,00' },
+        { startPrice: 'R$ 10,00', endPrice: 'R$ 770,00' }
+    ]
+
     return (
-        <div className="Price">
-            <h1>Price</h1>
+        <div className="price">
+            <h3 className="title">Preço</h3>
+            <div className="prices">
+                {prices.map((price, index) => (
+                    <div key={index} className={ `price-button ${price?.active ? 'active' : ''}` }>
+                        <span>{price.startPrice}</span> - <span>{price.endPrice}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
