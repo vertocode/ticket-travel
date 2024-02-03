102d34aa53ae8f03b2bd1fbd094e48536d3f7efc
vertocode <evertonvanoni1@gmail.com>
2024-02-03 18:04:44 -0300

    feat: allow the user to change the filtered price

--- components/Filters/Price/Price.tsx
+++ components/Filters/Price/Price.tsx
@@ -1,20 +1,36 @@
-import { ReactElement } from 'react'
+'use client'
+
+// types
+import { ReactElement, } from 'react'
+
+// styles
 import '@/styles/components/Filters/Price/Price.scss'
 
+// hooks
+import { useMemo,useState } from 'react'
+
 const Price = (): ReactElement => {
-	const prices = [
-		{ startPrice: 'R$ 10,00', endPrice: 'R$ 770,00', active: true },
+	const [selectedPriceIndex, setSelectedPriceIndex] = useState(0)
+	const prices = useMemo(() => [
+		{ startPrice: 'R$ 10,00', endPrice: 'R$ 770,00' },
 		{ startPrice: 'R$ 10,00', endPrice: 'R$ 770,00' },
 		{ startPrice: 'R$ 10,00', endPrice: 'R$ 770,00' },
 		{ startPrice: 'R$ 10,00', endPrice: 'R$ 770,00' }
-	]
+	].map((price, index) =>  ({
+		...price,
+		active: index === selectedPriceIndex
+	})), [selectedPriceIndex])
 
 	return (
 		<div className="price">
 			<h3 className="title">Preço</h3>
 			<div className="prices">
 				{prices.map((price, index) => (
-					<div key={index} className={ `price-button ${price?.active ? 'active' : ''}` }>
+					<div
+						key={index}
+						className={ `price-button ${price?.active ? 'active' : ''}` }
+						onClick={ () => setSelectedPriceIndex(index) }
+					>
 						<span>{price.startPrice}</span> - <span>{price.endPrice}</span>
 					</div>
 				))}
