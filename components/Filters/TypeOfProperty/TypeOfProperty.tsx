834950a44b57fb0061213d5d32d5c83eee0f8924
vertocode <evertonvanoni1@gmail.com>
2024-02-03 18:17:36 -0300

    feat: allow the user change the type of property

--- components/Filters/TypeOfProperty/TypeOfProperty.tsx
+++ components/Filters/TypeOfProperty/TypeOfProperty.tsx
@@ -1,26 +1,45 @@
+// types
 import { ReactElement } from 'react'
-import Image from 'next/image'
+
+// styles
 import '@/styles/components/Filters/TypeOfProperty/TypeOfProperty.scss'
 
 // images
 import casaImg from '@/public/imgs/casa.svg'
+import casaDisabledImg from '@/public/imgs/casa-disabled.svg'
 import apartamentoImg from '@/public/imgs/apartamento.svg'
+import apartamentoDisabledImg from '@/public/imgs/apartamento-disabled.svg'
 import hotelImg from '@/public/imgs/hotel.svg'
+import hotelDisabledImg from '@/public/imgs/hotel-disabled.svg'
+
+// components
+import Image from 'next/image'
+
+// hooks
+import { useMemo, useState } from 'react'
 
 const TypeOfProperty = (): ReactElement => {
-	const typeOfPropertyConfig = [
-		{ type: 'casa', quantity: '346', src: casaImg, active: true },
-		{ type: 'Apartamento', quantity: '234', src: apartamentoImg },
-		{ type: 'Hotel', quantity: '23', src: hotelImg },
-	]
+	const [selectedPropertyIndex, setSelectedPropertyIndex] = useState(0)
+	const typeOfProperties = useMemo(() => [
+		{ type: 'casa', quantity: '346', src: casaImg, srcDisabled: casaDisabledImg },
+		{ type: 'Apartamento', quantity: '234', src: apartamentoImg, srcDisabled: apartamentoDisabledImg },
+		{ type: 'Hotel', quantity: '23', src: hotelImg, srcDisabled: hotelDisabledImg },
+	].map((property, index) => ({
+		...property,
+		active: index === selectedPropertyIndex
+	})), [selectedPropertyIndex])
 
 	return (
 		<div className="type-of-property">
 			<h3 className="title">Tipo de propriedade</h3>
 			<div className="types">
-				{typeOfPropertyConfig.map((typeOfProperty, index) => (
-					<div key={index} className={`type-button ${typeOfProperty?.active ? 'active' : ''}`}>
-						<Image height={ 24 } width={ 24 } src={typeOfProperty.src} alt={typeOfProperty.type} />
+				{typeOfProperties.map((typeOfProperty, index) => (
+					<div
+						key={index}
+						className={`type-button ${typeOfProperty?.active ? 'active' : ''}`}
+						onClick={() => setSelectedPropertyIndex(index)}
+					>
+						<Image height={ 24 } width={ 24 } src={ typeOfProperty?.active ? typeOfProperty.src : typeOfProperty.srcDisabled} alt={typeOfProperty.type} />
 						<span className="type">{typeOfProperty.type}</span>
 						<span className="quantity">({typeOfProperty.quantity})</span>
 					</div>
